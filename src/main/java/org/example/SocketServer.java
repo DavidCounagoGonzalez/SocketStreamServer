ca11399b4ad792b6c3bb8ea50bb964f30a9988a6
DavidCounagoGonzalez <dcounagogonzalez@danielcastelao.org>
2023-03-11 17:11:17 +0100

    No quiere estar solo y rompe infinito

--- src/main/java/org/example/SocketServer.java
+++ src/main/java/org/example/SocketServer.java
@@ -1,51 +1,69 @@
 package org.example;
 
+
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Observable;
 
 public class SocketServer {
+    static Socket sc;
 
     public static void main(String[] args){
-        try{
-            System.out.println("Creando socket servidor");
+        new InterfazServer();
 
-            ServerSocket serverSocket=new ServerSocket();
+        try {
+            ServerSocket servidor = new ServerSocket(5554);
 
-            System.out.println("Realizando el bind");
 
-            InetSocketAddress addr=new InetSocketAddress("10.0.9.15",5555);
-            serverSocket.bind(addr);
+            System.out.println("Incia el server");
 
-            System.out.println("Aceptando conexiones");
+            while(true){
+                sc = servidor.accept();
 
-            Socket newSocket= serverSocket.accept();
+                DataInputStream in = new DataInputStream(sc.getInputStream());
+                DataOutputStream out = new DataOutputStream(sc.getOutputStream());
 
-            System.out.println("Conexion recibida");
+                out.writeUTF("Dame un nombre para identidicarte");
+                String nombreCliente = in.readUTF();
 
-            InputStream is=newSocket.getInputStream();
-            DataInputStream dis = new DataInputStream(is);
-            OutputStream os=newSocket.getOutputStream();
+                HiloServer hilo  = new HiloServer(in, out, nombreCliente);
+                hilo.start();
 
-            String mensaje = dis.readUTF();
+                System.out.println("Creada la conexión con el cliente " + nombreCliente);
+            }
 
-            System.out.println("Mensaje recibido: "+mensaje);
 
-            System.out.println("Cerrando el nuevo socket");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-            newSocket.close();
+    }
 
-            System.out.println("Cerrando el socket servidor");
+    public static void envio(){
+        try {
+            DataOutputStream out = new DataOutputStream(sc.getOutputStream());
+            out.writeUTF("Server --> "  + InterfazServer.txtEscribe.getText());
+            InterfazServer.txtEscribe.setText("");
 
-            serverSocket.close();
 
-            System.out.println("Terminado");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        }catch (IOException e) {
+    public static void recibo(){
+        try {
+            InputStream dis = sc.getInputStream();
+            DataInputStream is = new DataInputStream(dis);
+            String recibido = is.readUTF();
+            InterfazServer.chat.append(recibido+"\n");
+        }catch(IOException exc){
+            exc.printStackTrace();
         }
     }
 }
